f92a0eaf6b7dcdf93f0e1d1a1ae8d6808181d9b1
Reuben Tart <rtart@blurpd.com>
2024-08-08 20:20:39 -0400

    initial commit

---
+++ pen_functions.py
@@ -0,0 +1,80 @@
+from PIL import Image, ImageDraw, ImageFont
+jp_font = ImageFont.truetype("BIZ-UDGothicR.ttc", 50)
+en_font = ImageFont.truetype("arial.ttf", 50)
+furigana = ImageFont.truetype("BIZ-UDGothicR.ttc", 20)
+frame = (1920, 1080)
+
+
+def _int(thing_to_int):
+    try:
+        return int(thing_to_int)
+    except ValueError:
+        return thing_to_int
+
+
+def from_bottom(y):
+    """takes the y position from the bottom of the canvas and returns the position from the top
+    (example: 20 pixels from the bottom is 1900 pixels from the top)"""
+    return 1080-y
+
+
+def center_start(text, font=jp_font):
+    """returns where the left edge of text would be if it is centered on the canvas"""
+    text_width = font.getlength(text)
+    return frame[0]/2 - text_width/2
+
+
+class TextWriter:
+    def __init__(self):
+        # make the canvas and pen
+        self.canvas = Image.new("RGBA", frame, color=(0, 0, 0, 0))
+        self.pen = ImageDraw.Draw(self.canvas)
+
+        # cursor control
+        self.cursor_x = 0
+        self.cursor_y = 0
+
+    def set_pos(self, position):
+        """set the cursor position. Accepts "center" instead of a coordinate
+        update: why would you want to set to center, you would just type to the right"""
+        if (not isinstance(position, tuple)) or (len(position) != 2):
+            raise TypeError(f"set_pos requires a tuple position of length 2, instead received {str(type(position))}.")
+
+        if not (
+                (isinstance(_int(position[0]), int) or position[0] == "center") and
+                (isinstance(_int(position[1]), int) or position[1] == "center")
+        ):
+            raise TypeError(f"one of the positions set is invalid. got: {position}")
+
+        self.cursor_x = frame[0]/2 if position[0] == "center" else position[0]
+        self.cursor_y = frame[1]/2 if position[1] == "center" else position[1]
+
+    def scrawl(self, text, color, font=en_font, stroke=True):
+        """write the text and move the cursor to where the text ends"""
+        print(f"writing {text} with font {font.getname()[0]}")
+
+        self.pen.text(
+            (self.cursor_x, self.cursor_y),     # position
+            text, fill=color, font=font,        # text
+            stroke_width=5 if stroke else 0, stroke_fill=(0, 0, 0, 255)  # stroke
+        )
+
+        text_length = font.getlength(text)
+        self.cursor_x += text_length
+
+    def furigana_scrawl(self, jp_text, reading, color, stroke=True):
+        """scrawl text and add furigana above it"""
+        for i, char in enumerate(jp_text):
+            # if char is kana
+            if char in "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン":
+                self.scrawl(char, color, stroke=stroke, font=jp_font)
+            # if we reach a kanji
+            else:
+                # write the furigana
+                self.pen.text(
+                    (self.cursor_x, self.cursor_y-30),         # position
+                    reading, fill=color, font=furigana,     # text
+                    stroke_width=5 if stroke else 0, stroke_fill=(0, 0, 0, 255)  # stroke
+                )
+                self.scrawl(jp_text[i:], color, stroke=stroke, font=jp_font)
+                break
---
+++ subtitle_maker.py
@@ -0,0 +1,55 @@
+from pen_functions import *
+import re
+
+
+def extract(text):
+    """extract text in parentheses"""
+    # Find all matches and remove them from the original text
+    removed_text = re.findall(r'\(.*?\)', text)
+    cleaned_text = re.sub(r'\(.*?\)', '', text)
+
+    reading = removed_text[0].replace("(", "").replace(")", "") if removed_text else ""
+
+    # Return the cleaned text and the removed text
+    return cleaned_text, reading
+
+
+def make_subtitle(jp_text, en_text, color_palette):
+    """
+    makes the subtitle out of jp and en text.
+    :arg jp_text: each section should be split up by pipes (|), with furigana after kanji. example: 皆(みな)さん|どうも
+    :arg en_text: like jp text, sections should be split by pipes, but each section should start with its position in
+                  the Japanese sentence. For example, the above JP would be (2)Hello|(1)everyone!
+    :arg color_palette: the colors that are cycled through to color code words. should be a list of tuples: (r, g, b, a)
+    """
+    cursor = TextWriter()
+    clean_jp = re.sub(r"\(.*?\)", "", jp_text).replace("|", "")
+    cursor.set_pos((center_start(clean_jp, font=jp_font), from_bottom(160)))
+
+    # write japanese text
+    for i, section in enumerate(jp_text.split("|")):
+        clean_jp, reading = extract(section)
+
+        cursor.furigana_scrawl(clean_jp, reading, color_palette[i])
+
+    clean_en = re.sub(r"\(.*?\)", "", en_text).replace("|", "")
+    cursor.set_pos((center_start(clean_en, font=en_font), from_bottom(80)))
+
+    # write english text
+    for section in en_text.split("|"):
+        # add a space to the end
+        section = section + " "
+
+        # extract the index
+        clean_en, i = extract(section)
+
+        cursor.scrawl(clean_en, color_palette[int(i)-1])
+
+    cursor.canvas.show()
+
+
+if __name__ == "__main__":
+    make_subtitle(
+        "皆(みな)さん|どうもこんばんは", "(2)Good morning|(1)everyone!",  # jp and en text
+        [(255, 227, 253, 255), (177, 152, 245, 255), (158, 215, 255, 255)]  # color palette
+    )
